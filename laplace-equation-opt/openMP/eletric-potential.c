e5780803d39f278d0668df453e68eb3d7028540b
Pedro Henrique Santos Cunha <hnrqpdr.sc@gmail.com>
2022-01-26 13:56:22 -0300

    projeto 1

--- laplace-equation-opt/openMP/eletric-potential.c
+++ laplace-equation-opt/openMP/eletric-potential.c
@@ -8,13 +8,11 @@
 #define N 1000
 #define tolerance 0.1
 #define maxIt 100000000
-#define errorTolerance 1e-8
 #define potencialInterno -50.0
 #define potencialExterno 100.0
 #define raioInterno 1.0
 #define raioExterno 3.0
 #define theta 0.4
-#define numberThreads 8
 
 // Dimensões da malha
 
@@ -23,83 +21,64 @@
 #define yInicial -5.0
 #define yFinal 5.0
 
-void setContour(double **v, double dx, double dy, int chunk){
+void setContour(double **v, double dx, double dy, int chunk, int numberThreads){
     int i, j;
     double x, y, r;
 
-    omp_set_num_threads(numberThreads);
-    #pragma omp parallel private(x, y, r, i, j)
-    {
-
         for(i = 0; i < N/2; i++){
+            for(j = 0; j < N/2; j++){
+                x = xInicial + i*dx;
+                y = yInicial + j*dy;
+                r = sqrt(x*x + y*y);
 
-            #pragma omp parallel shared(v)
-            {
-                #pragma omp for schedule(dynamic, chunk)
-
-                for(j = 0; j < N/2; j++){
-                    x = xInicial + i*dx;
-                    y = yInicial + j*dy;
-                    r = sqrt(x*x + y*y);
-
-                    if(fabs(raioExterno - r) < tolerance){
-                        v[i][j] = potencialExterno;
-                        v[N-i-1][j] = potencialExterno;
-                        v[i][N-j-1] = potencialExterno;
-                        v[N-i-1][N-j-1] = potencialExterno;
-                    }
-                    else{
-
-                        if(fabs(raioInterno - r) < tolerance){
-                            v[i][j] = potencialInterno;
-                            v[N-i-1][j] = potencialInterno;
-                            v[i][N-j-1] = potencialInterno;
-                            v[N-i-1][N-j-1] = potencialInterno;
-                        }
-                    } 
+                if(fabs(raioExterno - r) < tolerance){
+                    v[i][j] = potencialExterno;
+                    v[N-i-1][j] = potencialExterno;
+                    v[i][N-j-1] = potencialExterno;
+                    v[N-i-1][N-j-1] = potencialExterno;
                 }
-            }
+                else{
+
+                    if(fabs(raioInterno - r) < tolerance){
+                        v[i][j] = potencialInterno;
+                        v[N-i-1][j] = potencialInterno;
+                        v[i][N-j-1] = potencialInterno;
+                        v[N-i-1][N-j-1] = potencialInterno;
+                }
+            } 
         }
     }
 }
 
-void setAngleCut(double **v, double dx, double dy, int chunk){
+void setAngleCut(double **v, double dx, double dy, int chunk, int numberThreads){
     int i, j;
     double x, y, r, ang;
 
-    omp_set_num_threads(numberThreads);
-    #pragma omp parallel private(x, y, r, ang, i, j)
-    {
-        for(i = 0; i < N/2; i++){
+    
+    for(i = 0; i < N/2; i++){
+        for(j = N/2; j < N; j++){
+                x = xInicial + i*dx;
+                y = yInicial + j*dy;
+                r = sqrt(x*x + y*y);
+                ang = acos(x/r);
 
-            #pragma omp parallel shared(v)
-            {
-                #pragma omp for schedule(dynamic, chunk)
-
-                for(j = N/2; j < N; j++){
-                    x = xInicial + i*dx;
-                    y = yInicial + j*dy;
-                    r = sqrt(x*x + y*y);
-                    ang = acos(x/r);
-
-                    if(fabs(raioExterno - r) < tolerance && ang < theta){
-                        v[i][j] = 0;
-                        v[i][N-j-1] = 0;
-                    }
-                    else{
-                        if(fabs(raioInterno - r) < tolerance && ang < theta){
-                            v[i][j] = 0;
-                            v[i][N-j-1] = 0;
-                        }
-                    }
+            if(fabs(raioExterno - r) < tolerance && ang < theta){
+                v[i][j] = 0;
+                v[i][N-j-1] = 0;
+            }
+            else{
+                if(fabs(raioInterno - r) < tolerance && ang < theta){
+                    v[i][j] = 0;
+                    v[i][N-j-1] = 0;
                 }
             }
-        }
+        }   
     }
 }
 
 void getResults(double **v, double dx, double dy){
 
+    int i, j;
     double x, y;
     FILE *arquivo;
 
@@ -107,8 +86,8 @@ void getResults(double **v, double dx, double dy){
 
     fprintf(arquivo, "x\ty\tz\n");
 
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
+    for(i = 0; i < N; i++){
+        for(j = 0; j < N; j++){
             x = xInicial + i*dx;
             y = yInicial + j*dy;
             fprintf(arquivo, "%g %g %g\n", x, y, v[i][j]);
@@ -118,63 +97,24 @@ void getResults(double **v, double dx, double dy){
     fclose(arquivo);
 }
 
-void potentialCalc(double **v, double **v_old, double dx, double dy, int chunk){
-    int i, j;
+void finiteDifference(double **v, double **v_old, double dx, double dy, int chunk, int numberThreads){
+    
+    int a, b, i, j, k;
     double x, y, r;
 
     omp_set_num_threads(numberThreads);
 
-    #pragma omp parallel private(x, y, r, i, j)
+    #pragma omp parallel private(a, b, x, y, r, i, j, k) shared(v, v_old)
     {
-        for(i = 1; i < N-1; i++){
+        for(k = 0; k < maxIt; k++){
 
-            #pragma omp parallel shared(v, v_old)
-            {
-                #pragma omp for schedule(dynamic, chunk)
-
-                for(j = N/2; j < N-1; j++){
-
-                    x = xInicial + i*dx;
-                    y = yInicial + j*dy;
-                    r = sqrt(x*x + y*y);
-                    if(fabs(raioExterno - r) < tolerance && (v[i][j] == potencialExterno )){
-                        v[i][j] = potencialExterno;
-                    }
-                    else{
-                        if(fabs(raioInterno - r) < tolerance && (v[i][j] == potencialInterno)){
-                            v[i][j] = potencialInterno;
-                        }
-                        else{
-                            v[i][j] = (1.0/4.0)*(v_old[i+1][j] + v_old[i-1][j] + v_old[i][j+1] + v_old[i][j-1]); 
-                            v[i][N-j-1] = v[i][j];
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-void finiteDifference(double **v, double **v_old, double dx, double dy, int chunk){
-    
-    int i, j;
-    double x, y, r;
-
-    #pragma omp parallel private(x, y, r, i, j, k) shared(v, v_old)
-    {
-
-    for(int k = 0; k < maxIt; k++){
-
-        for(int a = 0; a < N; a++)
-		for(int b = 0; b < N; b++)
-			v_old[a][b] = v[a][b];
-		
-        omp_set_num_threads(numberThreads);
+            for(a = 0; a < N; a++)
+            for(b = 0; b < N; b++)
+                v_old[a][b] = v[a][b];
+            
+            #pragma omp for schedule(dynamic, chunk) collapse(2)
 
             for(i = 1; i < N-1; i++){
-
-                #pragma omp for schedule(dynamic, chunk)
-
                 for(j = N/2; j < N-1; j++){
 
                     x = xInicial + i*dx;
@@ -200,28 +140,29 @@ void finiteDifference(double **v, double **v_old, double dx, double dy, int chun
 
 int main(int argc, char *argv[]){
 
-    int i, j, k, chunk;
+    int i, j, k, chunk, numberThreads;
     double dx, dy, d, x, y, r;
     double **v, **v_old;
 
+    numberThreads = atoi(argv[1]);
     chunk = N/numberThreads;
 
     dx = (xFinal - xInicial)/N;
     dy = (yFinal - yInicial)/N;
 
     v = malloc(N*sizeof(double *));
-    for(int i=0;i<N;i++){
+    for(i=0;i<N;i++){
         v[i] = malloc(N*sizeof(double));
     }
 
     v_old = malloc(N*sizeof(double *));
-    for(int i=0;i<N;i++){
+    for(i=0;i<N;i++){
         v_old[i] = malloc(N*sizeof(double));
     }
 
-    setContour(v, dx, dy, chunk);
-    setAngleCut(v, dx, dy, chunk);
-    finiteDifference(v, v_old, dx, dy, chunk);
+    setContour(v, dx, dy, chunk, numberThreads);
+    setAngleCut(v, dx, dy, chunk, numberThreads);
+    finiteDifference(v, v_old, dx, dy, chunk, numberThreads);
     getResults(v, dx, dy);
 
     free(v);
